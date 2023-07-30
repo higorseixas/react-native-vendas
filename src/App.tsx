96df8f3cb80e7959763da655f6503f8f710b6b78
Higor Clem Seixas <higor.c.seixas@gmail.com>
2023-07-30 17:40:29 -0300

    feat: config project

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { SafeAreaView, Text, View } from "react-native";
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>Hello World!</Text>
+      </View>
+    </SafeAreaView>
+
+  );
+};
+
+export default App;
